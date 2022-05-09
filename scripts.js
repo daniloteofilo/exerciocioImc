f8c9a8cb3204c7df0aefe6001ceb30d7cc06fcea
Danilo Teofilo <danigolaseu@gmail.com>
2022-05-08 21:04:33 -0300

    Subindo respositório.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let nome = "Naiani";
+
+let peso = 40;
+
+let altura = 1.59;
+
+let imc = peso / (altura**2);
+
+function obesidade(){
+    if (imc>= 25) {
+        document.write("O Imc de " + nome + " é aproximadamente " + Math.round(imc) + " indicando que " + nome + " está com sobrepeso.");
+    }
+
+    else if (imc <25 && imc >= 18) {
+        document.write("O Imc de " + nome + " é aproximadamente " + Math.round(imc) + " indicando que " + nome + " está na faixa de peso ideal.");
+    }
+
+    else if (imc <18) {
+        document.write("O Imc de " + nome + " é aproximadamente " + Math.round(imc) + " indicando que " + nome + " está com o peso abaixo do ideal.");
+    }
+}
+
+obesidade();
